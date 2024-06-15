44ee023fb6bb2dda93a56ac3fee723971c686fca
AgnellaForc <agneseforcella@yahoo.com>
2024-06-15 15:58:46 +0200

    data set xlsx

---
+++ NPS_magenta.R
@@ -0,0 +1,13 @@
+#Group project AI in Marketing
+#GOAL: Generate insights on how to improve customer experience
+
+#1. Run exploratory Analysis to understand the data and the problem you are trying to solve
+
+#2. Predict NPS (Net Promoter Score) using structured data such as categorical, numeric features
+
+#3. Predict NPS (Net Promoter Score) using structured & unstructured data (text)
+
+#4. How well can you predict customer churn from the data? 
+
+#5. How can the firm improve customer experience? Derive direct implications based on your findings in 1-4 
+
