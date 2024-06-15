ef0dc48878e112e562b418e12310748b002ae69f
AgnellaForc <agneseforcella@yahoo.com>
2024-06-15 16:26:03 +0200

    Update NPS_magenta.R

--- NPS_magenta.R
+++ NPS_magenta.R
@@ -1,5 +1,39 @@
 #Group project AI in Marketing
+
+#install packages
+install.packages("DataExplorer") #cant install it, why?
+#.....(add packages here)
+
+#load packages
+library(readxl)
+library(dplyr)
+library(DataExplorer)
+library(ggplot2)
+#.....(add packages here)
+
+#load dataset
+NPS_Data_Magenta <- read_excel("~/Documents/GitHub/Magenta/NPS Data Magenta.xlsx")
+View(NPS_Data_Magenta)
+
+#convert chaarcter into factor
+NPS_Data_Magenta <- NPS_Data_Magenta %>%
+  mutate_if(is.character,factor)
+
+#visualize the data #CANT PLOT THE DATA
+# Set the size of plots
+options(repr.plot.width=12, repr.plot.height=8)
+# Plot the introductory plot
+plot_intro(NPS_Data_Magenta)
+# Plot the bar plot
+plot_bar(NPS_Data_Magenta, by = "Churn")
+
+#plot with ggplot
+
+
+            #-------------------------------------------
 #GOAL: Generate insights on how to improve customer experience
+
+
 
 #1. Run exploratory Analysis to understand the data and the problem you are trying to solve
 
